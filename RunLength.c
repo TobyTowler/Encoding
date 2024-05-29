e92d0300a3eeb695e31b8cd5fd1500aaa31fe17f
TobyTowler <tobytowler11@gmail.com>
2024-05-29 17:31:31 +0100

    RLE length checker

--- RunLength.c
+++ RunLength.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 void Encode(char str[], char res[], int len, int *resLen) {
 
@@ -6,7 +7,6 @@ void Encode(char str[], char res[], int len, int *resLen) {
     for (size_t i = 1; i < len; i++) {
         last = str[i - 1];
         current = str[i];
-        // printf("%c, %c \n", last, current);
         if (last != current) {
             res[2 * num] = (i - startingPos);
             res[2 * num + 1] = last;
@@ -18,9 +18,10 @@ void Encode(char str[], char res[], int len, int *resLen) {
             res[2 * num] = (i - startingPos + 1);
             res[2 * num + 1] = str[startingPos];
             num++;
+            res[2 * num] = -1;
+            *resLen = 2 * num;
         }
     }
-    *resLen = 2 * num;
 }
 
 void Decode(char encoded[], int encodedLen, char decoded[], int starterLen) {
@@ -30,17 +31,20 @@ void Decode(char encoded[], int encodedLen, char decoded[], int starterLen) {
     for (size_t i = 0; i < encodedLen; i += 2) {
         count = encoded[i];
         letter = encoded[i + 1];
-
         for (size_t o = 0; o < count; o++) {
             decoded[pos++] = letter;
         }
+
+        if (count + 2 == -1)
+            break;
     }
     decoded[pos] = '\0';
 }
 
 int main() {
     int starterLen = 14, encodedLen = 2 * starterLen;
-    char starter[14] = "AAAABBCAAFFFFE";
+    // char starter[14] = "AAAABBCAAFFFFE";      //Shorter encoded
+    char starter[14] = "ABCDEFGHJKLMNO"; // Longer encoded
     char encoded[2 * 14] = "";
     char decoded[14] = "";
 
@@ -61,6 +65,11 @@ int main() {
     for (size_t i = 0; i < starterLen; i++) {
         printf("%c", decoded[i]);
     }
+
+    if (encodedLen < starterLen) {
+        printf("\nEncoding Shorter");
+    } else
+        printf("\nEncoding longer, use starter list");
 
     printf("\n");
 }
